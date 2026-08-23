1cf3f126a2c8971840c6bdcd8379ea253d80c04d
Rosa ccr <Rosa.ccr@ReliancePower.com>
2026-08-23 17:23:54 +0530

    Update UPSLDC data hourly from UCE PC

--- update_upsldc.bat
+++ update_upsldc.bat
@@ -7,10 +7,8 @@ git add .
 
 git diff --cached --quiet
 if %errorlevel%==0 (
-    echo No changes to commit.
-) else (
-    git commit -m "Update UPSLDC data hourly from UCE PC"
-    git push
+    exit /b 0
 )
 
-exit
+git commit -m "Update UPSLDC data hourly from UCE PC"
+git push
